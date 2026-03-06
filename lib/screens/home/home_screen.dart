3c0cc0d341e9479cae6a3a5235c91304d33dc471
rachmatrisky <rachmatrisky5@gmail.com>
2026-03-06 23:46:26 +0800

    Add missing import for routes in HomeScreen

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 // lib/screens/home/home_screen.dart
 import 'package:flutter/material.dart';
+import 'package:flutter_ml_applied_app/config/routes.dart';
 import 'package:flutter_ml_applied_app/providers/image_provider.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
